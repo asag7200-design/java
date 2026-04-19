fee79babfd6048a04a18592d8cd4154f381f3617
asag7200-design <asag7200@gmail.com>
2026-04-19 22:07:03 +0530

    intialize

---
+++ MarksGrade.java
@@ -0,0 +1,13 @@
+public class MarksGrade {
+    public static void main(String[] args) {
+        int marks = 75;
+
+        if (marks > 90) {
+            System.out.println("Grade A");
+        } else if (marks > 60) {
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Grade C");
+        }
+    }
+}
