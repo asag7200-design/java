a8f9ae1f096d5eff9553036868046939020c8b41
asag7200-design <asag7200@gmail.com>
2026-04-23 22:29:24 +0530

    initial

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class FactorialNumber {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -11,10 +11,7 @@ public class FactorialNumber {
         long factorial = 1;
 
         for (int i = 1; i <= num; i++) {
-            factorial = factorial * i;
-        }
-
-    
+            factorial = factorial * i; 
         System.out.println("Factorial of " + num + " is: " + factorial);
 
         sc.close();
