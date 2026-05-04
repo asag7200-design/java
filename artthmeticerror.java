e1074a034a6d65b7dee1f3ca78692dafe73ba52d
asag7200-design <asag7200@gmail.com>
2026-05-04 15:06:38 +0530

    ArithmeticError

---
+++ artthmeticerror.java
@@ -0,0 +1,27 @@
+public class artthmeticerror {
+    public static void main(String[] args) {
+        int a = 10;
+        int c = 0;
+        //exception
+
+        try{
+            int result = a / c;
+            
+        System.out.println(result);
+        }
+        catch(ArithmeticException e ){
+            
+        System.out.println("can't divided by zero");
+
+        }finally{
+            
+            
+        System.out.println("Changed value of c");
+        }
+
+        System.out.println("Program ended");
+        
+        
+    }
+    
+}
