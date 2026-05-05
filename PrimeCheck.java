c0aa0fef7fd95db70c533f9884d95da3f14ef4e1
asag7200-design <asag7200@gmail.com>
2026-05-05 10:36:25 +0530

    Pyramid Shape

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class PrimeCheck {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
-        int num = x.nextInt();
+        try (Scanner x = new Scanner(System.in)) {
+            int num = x.nextInt();
  if (num%2==0) {System.out.println("prime");
-    
+   
  } else {System.out.println("odd");
-    
+   
  }
+        }
     }
 }
